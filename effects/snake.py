36bc504f9cd6fd42974732c515ff4c846050213e
jbos <jeremias.bosch@gmail.com>
2016-01-17 21:44:20 +0000

    Hack snake effect

--- effects/snake.py
+++ effects/snake.py
@@ -1,41 +1,199 @@
 import hyperion
-import time
 import colorsys
+import socket
+ 
 
-# Get the parameters
-rotationTime = float(hyperion.args.get('rotation-time', 10.0))
-color = hyperion.args.get('color', (255,0,0))
-percentage = int(hyperion.args.get('percentage', 10))
 
-# Check parameters
-rotationTime = max(0.1, rotationTime)
-percentage = max(1, min(percentage, 100))
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Process parameters
-factor = percentage/100.0
-hsv = colorsys.rgb_to_hsv(color[0]/255.0, color[1]/255.0, color[2]/255.0)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
 
-# Initialize the led data
-snakeLeds = max(1, int(hyperion.ledCount*factor))
-ledData = bytearray()
+# import required modules
+import RPi.GPIO as GPIO
+import time
 
-for i in range(hyperion.ledCount-snakeLeds):
-	ledData += bytearray((0, 0, 0))
+# define GPIO pin with button
+GPIOFrontLight = 13
+GPIOSwitchAmbianceMode = 11
+GPIODimmer = 15
 
-for i in range(1,snakeLeds+1):
-	rgb = colorsys.hsv_to_rgb(hsv[0], hsv[1], hsv[2]/i)
-	ledData += bytearray((int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255)))
+# main function
+def main():
+  try:
+    # use GPIO pin numbering convention
+    GPIO.setmode(GPIO.BOARD)
 
-# Calculate the sleep time and rotation increment
-increment = 3
-sleepTime = rotationTime / hyperion.ledCount
-while sleepTime < 0.05:
-	increment *= 2
-	sleepTime *= 2
-increment %= hyperion.ledCount
+    # set up GPIO pin for input
+    GPIO.setup(GPIOFrontLight, GPIO.IN)
+    GPIO.setup(GPIOSwitchAmbianceMode, GPIO.IN)
+    GPIO.setup(GPIODimmer, GPIO.IN)
 
-# Start the write data loop
-while not hyperion.abort():
-	hyperion.setColor(ledData)
-	ledData = ledData[increment:] + ledData[:increment]
-	time.sleep(sleepTime)
+    GPIO.setup(GPIOFrontLight, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup(GPIOSwitchAmbianceMode, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup(GPIODimmer, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+
+    frontLightStatus = 0  # 0 = off , 1 = on
+    ambianceLightStatus = 0 # 0 = off, 1 = on
+
+    ambianceMode = 0 # 0 = normal ambiance light , 1 = tv ambiance mode
+
+    dimmerValue = 10 # 0 to 10
+
+    UDP_IP = ""
+    UDP_PORT = 8888
+
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    sock.bind((UDP_IP, UDP_PORT))
+
+    
+    ledDataWandOn = bytearray()
+    ledDataLampenOn = bytearray()
+    ledDataWandOff = bytearray()
+    ledDataLampenOff = bytearray()
+    hsv = colorsys.rgb_to_hsv(250/255.0, 1.0, 158.0/255.0)
+    hsvFront = colorsys.rgb_to_hsv(250.0/255.0, 1.0, 158.0/255.0)
+
+
+    for i in range(1,152):
+      rgb = colorsys.hsv_to_rgb(hsv[0], hsv[1]*2, 1)
+      ledDataWandOn += bytearray((int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255)))
+      ledDataWandOff += bytearray((0,0,0))
+	 
+    for i in range(1,61):
+      rgb = colorsys.hsv_to_rgb(hsvFront[0], hsvFront[1]*2, hsvFront[2])
+      ledDataLampenOn += bytearray((int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255)))
+      ledDataLampenOff += bytearray((0,0,0))
+	
+    while not hyperion.abort():
+      # get GPIO value
+      if GPIO.input(GPIOSwitchAmbianceMode) and ambianceMode <> 1:
+        ambianceMode = 1
+     #   print("Button 2  pressed")
+
+      if not GPIO.input(GPIOSwitchAmbianceMode) and ambianceMode <> 0:
+        ambianceMode = 0
+        ambianceLightStatus = 0
+    #    print("Button 2 released")
+
+		
+      if GPIO.input(GPIOFrontLight) and frontLightStatus <> 1:
+        frontLightStatus = 1
+
+   #     print("Button 1  pressed")
+
+      if not GPIO.input(GPIOFrontLight) and frontLightStatus <> 0:
+        frontLightStatus = 0
+  #      print("Button 1 released")
+    
+    
+      if GPIO.input(GPIODimmer):
+        print("DIMMER 1")        
+
+        timeCounter = 0
+        
+        if GPIO.input(GPIODimmer) and ambianceLightStatus == 0:
+          print("Light on")
+          ambianceLightStatus = 1
+          ledData = ledDataWandOn
+
+          if frontLightStatus == 1:
+            ledData = ledData + ledDataLampenOn
+          else:
+            ledData = ledData + ledDataLampenOff
+
+          hyperion.setColor(ledData)
+
+
+        time.sleep(0.4)
+
+        while GPIO.input(GPIODimmer):
+          timeCounter = timeCounter + 1
+          print("TIME COUNTER")
+          if timeCounter > 10:
+            ambianceLightStatus = 0
+
+            ledData = ledDataWandOff
+
+            if frontLightStatus == 1:
+              ledData = ledData + ledDataLampenOn
+            else:
+              ledData = ledData + ledDataLampenOff
+
+            hyperion.setColor(ledData)
+
+ #           print("Light off")
+          
+          time.sleep(0.1)
+        
+        if ambianceLightStatus == 1 and timeCounter <=10:
+          print("DIMMING")
+          ledDataWandOn = bytearray()
+          dimmerValue = dimmerValue - 1
+
+          if dimmerValue == -1:
+	    dimmerValue = 10
+
+          for i in range(1,152):
+      	    rgb = colorsys.hsv_to_rgb(hsv[0], hsv[1]*2, dimmerValue/10.0)
+            ledDataWandOn += bytearray((int(rgb[0]*255), int(rgb[1]*255), int(rgb[2]*255)))
+          
+        print("Dimmdone")
+
+      ledData = bytearray()
+      
+      if ambianceMode == 1:
+        srgb = bytearray(4096)
+        number,addr = sock.recvfrom_into(srgb) # buffer size is 
+     
+        ledDataServer = bytearray()
+        for i in range(1, number-3, 3):
+          ledDataServer += bytearray((srgb[i+1], srgb[i],srgb[i+2]))
+      
+        ledData = ledData ledDataServer
+  
+ #     for i in range(number/3+1,152):
+#        ledDataWandOff += bytearray((0,0,0))
+#      print len(ledDataWandOff)
+
+      else:
+
+        if ambianceLightStatus == 1:
+          ledData = ledData + ledDataWandOn
+        else:
+          ledData = ledData + ledDataWandOff
+
+      if frontLightStatus == 1:
+	ledData = ledData + ledDataLampenOn
+      else:
+        ledData = ledData + ledDataLampenOff   
+
+     
+      #data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+       
+      hyperion.setColor(ledData)
+	  
+      # wait 100ms
+      time.sleep(0.01)
+
+	  
+	
+	  
+  # reset GPIO settings if user pressed Ctrl+C
+  except KeyboardInterrupt:
+    print("Execution stopped by user")
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+  main()
+
+
+
+
